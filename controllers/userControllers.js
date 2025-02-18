7523f31cdf440ebf366dee50f191dcd769d67ffc
Rishi Raj <rishiraj1096@gmail.com>
2025-02-18 20:22:44 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -268,7 +268,7 @@ export const forgotPassword = catchAsyncError(async (req, res, next) => {
   //   "host"
   // )}/api/v1/password/reset/${resetToken}`;
 
-  const frontendurl = `http://localhost:5173/reset-password/${resetToken}`;
+  const frontendurl = `http://localhost:5173/auth/reset-password/${resetToken}`;
 
   const message = `Dear ${user.full_name},
 
