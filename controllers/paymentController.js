6924869f20c2133cc9340588916da1eb50fea955
rahulsd380 <rahulsd380@gmail.com>
2025-07-01 20:57:00 +0600

    Updated frontend url

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -55,7 +55,7 @@ export const paymentVerification = async (req, res) => {
     // });
 
     res.redirect(
-      `http://localhost:5173/payment-successful/${razorpay_payment_id}`
+      `https://pm-gurukul.vercel.app/payment-successful/${razorpay_payment_id}`
     );
   } else {
     res.status(400).json({
